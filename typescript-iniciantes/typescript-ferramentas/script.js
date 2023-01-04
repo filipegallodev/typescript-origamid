22945a0eed73e5d23795ecae83cfcd92daf98013
Filipe <fgallo_dev@hotmail.com>
2023-01-04 08:53:54 -0300

    TypeScript Ferramentas

---
+++ typescript-iniciantes/typescript-ferramentas/script.js
@@ -0,0 +1,5 @@
+const total = 100;
+function somar(a, b) {
+    return a + b;
+}
+console.log(somar(5, 10));
---
+++ typescript-iniciantes/typescript-ferramentas/script.ts
@@ -0,0 +1,7 @@
+const total = 100;
+
+function somar(a: number, b: number) {
+  return a + b;
+}
+
+console.log(somar(5, 10));
