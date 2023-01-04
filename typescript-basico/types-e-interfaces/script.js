92898826d23ec046eed02ea03e3f846ea6e09767
Filipe <fgallo_dev@hotmail.com>
2023-01-04 13:40:01 -0300

    Types e Interfaces 3

--- typescript-basico/types-e-interfaces/script.js
+++ typescript-basico/types-e-interfaces/script.js
@@ -1,29 +1,22 @@
 "use strict";
-let total = 20;
-total = "50";
-function preencherDados(dados) {
-    document.body.innerHTML += `
+// type NumberOrString = string | number;
+async function fetchProduct() {
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
+    const data = await response.json();
+    showProduct(data);
+}
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
     <div>
-      <h2>${dados.nome}</h2>
-      <p>R$ ${dados.preco}</p>
-      <p>Inclui teclado: ${dados.teclado ? "sim" : "não"}</p>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <div>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      </div>
+      <div>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      </div>
     </div>
   `;
 }
-const computador = {
-    nome: "Computador",
-    preco: 2000,
-    teclado: true,
-};
-preencherDados(computador);
-preencherDados({
-    nome: "Notebook",
-    preco: 2500,
-    teclado: false,
-});
-function pintarCategoria(categoria) {
-    if (categoria === "design") {
-        console.log("Pintar vermelho");
-    }
-}
-pintarCategoria("codigo");
--- typescript-basico/types-e-interfaces/script.ts
+++ typescript-basico/types-e-interfaces/script.ts
@@ -1,50 +1,89 @@
-type NumberOrString = string | number;
+// type NumberOrString = string | number;
 
-let total: NumberOrString = 20;
-total = "50";
+// let total: NumberOrString = 20;
+// total = "50";
 
-interface InterfaceProduto {
+// interface InterfaceProduto {
+//   nome: string;
+//   preco: number;
+//   teclado: boolean;
+// }
+
+// type TypeProduto = {
+//   nome: string;
+//   preco: number;
+//   teclado: boolean;
+// };
+
+// function preencherDados(dados: InterfaceProduto) {
+//   document.body.innerHTML += `
+//     <div>
+//       <h2>${dados.nome}</h2>
+//       <p>R$ ${dados.preco}</p>
+//       <p>Inclui teclado: ${dados.teclado ? "sim" : "não"}</p>
+//     </div>
+//   `;
+// }
+
+// const computador: InterfaceProduto = {
+//   nome: "Computador",
+//   preco: 2000,
+//   teclado: true,
+// };
+
+// preencherDados(computador);
+
+// preencherDados({
+//   nome: "Notebook",
+//   preco: 2500,
+//   teclado: false,
+// });
+
+// type Categorias = "design" | "codigo" | "descod";
+
+// function pintarCategoria(categoria: Categorias) {
+//   if (categoria === "design") {
+//     console.log("Pintar vermelho");
+//   }
+// }
+
+// pintarCategoria("codigo");
+
+interface Empresa {
   nome: string;
-  preco: number;
-  teclado: boolean;
+  fundacao: number;
+  pais: string;
 }
 
-type TypeProduto = {
+interface Product {
   nome: string;
   preco: number;
-  teclado: boolean;
-};
+  descricao: string;
+  garantia: string;
+  seguroAcidentes: boolean;
+  empresaFabricante: Empresa;
+  empresaMontadora: Empresa;
+}
 
-function preencherDados(dados: InterfaceProduto) {
-  document.body.innerHTML += `
+async function fetchProduct() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const data = await response.json();
+  showProduct(data);
+}
+
+fetchProduct();
+
+function showProduct(data: Product) {
+  document.body.innerHTML = `
     <div>
-      <h2>${dados.nome}</h2>
-      <p>R$ ${dados.preco}</p>
-      <p>Inclui teclado: ${dados.teclado ? "sim" : "não"}</p>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <div>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      </div>
+      <div>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      </div>
     </div>
   `;
 }
-
-const computador: InterfaceProduto = {
-  nome: "Computador",
-  preco: 2000,
-  teclado: true,
-};
-
-preencherDados(computador);
-
-preencherDados({
-  nome: "Notebook",
-  preco: 2500,
-  teclado: false,
-});
-
-type Categorias = "design" | "codigo" | "descod";
-
-function pintarCategoria(categoria: Categorias) {
-  if (categoria === "design") {
-    console.log("Pintar vermelho");
-  }
-}
-
-pintarCategoria("codigo");
