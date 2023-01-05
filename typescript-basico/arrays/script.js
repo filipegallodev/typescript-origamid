b1f997c79fc787dccef53fd69b1cef0b9270b547
Filipe <filipegallodev@hotmail.com>
2023-01-05 09:23:22 -0300

    Arrays 2

--- typescript-basico/arrays/script.js
+++ typescript-basico/arrays/script.js
@@ -1,15 +1,23 @@
 "use strict";
-const numeros = [10, 20, 30, 40, 50, 3];
-const valores = [10, "Taxas", 30, "Produto", 50, 3];
-function maiorQue10(data) {
-    return data.filter((n) => n > 10);
+// const numeros = [10, 20, 30, 40, 50, 3];
+// const valores = [10, "Taxas", 30, "Produto", 50, 3];
+async function fetchCursos() {
+    const response = await fetch("https://api.origamid.dev/json/cursos.json");
+    const data = await response.json();
+    mostrarCursos(data);
 }
-function filtrarValores(data) {
-    return data.filter((item) => typeof item === "number");
+fetchCursos();
+function mostrarCursos(data) {
+    data.forEach((curso) => {
+        document.body.innerHTML += `
+    <div>
+      <h2 style="color: ${curso.nivel === "iniciante" ? "#00f" : "#f00"}">${curso.nome}</h2>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? "Gratuito" : "Pago"}</p>
+      <p>Tags: ${curso.tags.join(", ")}</p>
+      <p>Aulas: ${curso.idAulas.join(" | ")}</p>
+    </div>
+    `;
+    });
 }
-console.log(maiorQue10(numeros));
-console.log(filtrarValores(valores));
-const dados = [
-    ["O Senhor dos Anéis", 80],
-    ["A Guerra dos Tronos", 120],
-];
--- typescript-basico/arrays/script.ts
+++ typescript-basico/arrays/script.ts
@@ -1,18 +1,53 @@
-const numeros = [10, 20, 30, 40, 50, 3];
-const valores = [10, "Taxas", 30, "Produto", 50, 3];
+// const numeros = [10, 20, 30, 40, 50, 3];
+// const valores = [10, "Taxas", 30, "Produto", 50, 3];
 
-function maiorQue10(data: Array<number>) {
-  return data.filter((n) => n > 10);
+// function maiorQue10(data: Array<number>) {
+//   return data.filter((n) => n > 10);
+// }
+
+// function filtrarValores(data: Array<number | string>) {
+//   return data.filter((item) => typeof item === "number");
+// }
+
+// console.log(maiorQue10(numeros));
+// console.log(filtrarValores(valores));
+
+// const dados = [
+//   ["O Senhor dos Anéis", 80],
+//   ["A Guerra dos Tronos", 120],
+// ];
+
+interface Curso {
+  nome: string;
+  nivel: "iniciante" | "avancado";
+  aulas: number;
+  gratuito: boolean;
+  horas: number;
+  idAulas: Array<number>;
+  tags: Array<string>;
 }
 
-function filtrarValores(data: Array<number | string>) {
-  return data.filter((item) => typeof item === "number");
+async function fetchCursos() {
+  const response = await fetch("https://api.origamid.dev/json/cursos.json");
+  const data = await response.json();
+  mostrarCursos(data);
 }
 
-console.log(maiorQue10(numeros));
-console.log(filtrarValores(valores));
+fetchCursos();
 
-const dados = [
-  ["O Senhor dos Anéis", 80],
-  ["A Guerra dos Tronos", 120],
-];
+function mostrarCursos(data: Array<Curso>) {
+  data.forEach((curso) => {
+    document.body.innerHTML += `
+    <div>
+      <h2 style="color: ${curso.nivel === "iniciante" ? "#00f" : "#f00"}">${
+      curso.nome
+    }</h2>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? "Gratuito" : "Pago"}</p>
+      <p>Tags: ${curso.tags.join(", ")}</p>
+      <p>Aulas: ${curso.idAulas.join(" | ")}</p>
+    </div>
+    `;
+  });
+}
