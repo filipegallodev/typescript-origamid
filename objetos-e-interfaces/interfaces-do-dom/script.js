76c94319b9adec0f5e7a47ee55192f96bb534213
Filipe <filipegallodev@hotmail.com>
2023-01-06 14:06:35 -0300

    Interfaces do DOM 1

---
+++ objetos-e-interfaces/interfaces-do-dom/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const video = document.querySelector("video");
+const video2 = document.querySelector("#videoprincipal");
+video?.volume;
+// video2?.volume;
+if (video2 instanceof HTMLVideoElement) {
+    video2.volume;
+}
---
+++ objetos-e-interfaces/interfaces-do-dom/script.ts
@@ -0,0 +1,9 @@
+const video = document.querySelector("video");
+const video2 = document.querySelector("#videoprincipal");
+
+video?.volume;
+// video2?.volume;
+
+if (video2 instanceof HTMLVideoElement) {
+  video2.volume;
+}
