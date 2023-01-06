9fd4d002fe5b5ffecc6de2e85bc89ac1c507d078
Filipe <filipegallodev@hotmail.com>
2023-01-06 14:24:21 -0300

    Interfaces do DOM 3

--- objetos-e-interfaces/interfaces-do-dom/script.js
+++ objetos-e-interfaces/interfaces-do-dom/script.js
@@ -6,15 +6,29 @@
 // if (video2 instanceof HTMLVideoElement) {
 //   video2.volume;
 // }
+// const links = document.querySelectorAll(".link");
+// console.log(links);
+// links.forEach((link) => {
+//   console.log(link);
+//   if (link instanceof HTMLAnchorElement) {
+//     console.log(link.href);
+//   }
+// });
+// const dados = [1, 2, 3];
+// const arrayLinks = Array.from(links);
+// const anchorLinks = arrayLinks.filter(
+//   (link) => link instanceof HTMLAnchorElement
+// );
+// console.log(anchorLinks);
 const links = document.querySelectorAll(".link");
-console.log(links);
+function modificaCorEBorda(link) {
+    link.style.color = "#333";
+    link.style.padding = "4px";
+    link.style.border = "2px solid #fb1";
+    link.style.borderRadius = "4px";
+}
 links.forEach((link) => {
-    console.log(link);
-    if (link instanceof HTMLAnchorElement) {
-        console.log(link.href);
+    if (link instanceof HTMLElement) {
+        modificaCorEBorda(link);
     }
 });
-const dados = [1, 2, 3];
-const arrayLinks = Array.from(links);
-const anchorLinks = arrayLinks.filter((link) => link instanceof HTMLAnchorElement);
-console.log(anchorLinks);
