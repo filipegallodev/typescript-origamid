2cf5907576ee178534e0857e449c740a23515997
Filipe <filipegallodev@hotmail.com>
2023-01-06 14:13:57 -0300

    Interfaces do DOM 2

--- objetos-e-interfaces/interfaces-do-dom/script.js
+++ objetos-e-interfaces/interfaces-do-dom/script.js
@@ -1,8 +1,20 @@
 "use strict";
-const video = document.querySelector("video");
-const video2 = document.querySelector("#videoprincipal");
-video?.volume;
+// const video = document.querySelector("video");
+// const video2 = document.querySelector("#videoprincipal");
+// video?.volume;
 // video2?.volume;
-if (video2 instanceof HTMLVideoElement) {
-    video2.volume;
-}
+// if (video2 instanceof HTMLVideoElement) {
+//   video2.volume;
+// }
+const links = document.querySelectorAll(".link");
+console.log(links);
+links.forEach((link) => {
+    console.log(link);
+    if (link instanceof HTMLAnchorElement) {
+        console.log(link.href);
+    }
+});
+const dados = [1, 2, 3];
+const arrayLinks = Array.from(links);
+const anchorLinks = arrayLinks.filter((link) => link instanceof HTMLAnchorElement);
+console.log(anchorLinks);
