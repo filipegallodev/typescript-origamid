aeba36cecf41121560a5eb135e42580803e35ee0
Filipe <filipegallodev@hotmail.com>
2023-01-06 09:38:02 -0300

    Instanceof 3

--- objetos-e-interfaces/instanceof/script.js
+++ objetos-e-interfaces/instanceof/script.js
@@ -94,3 +94,7 @@
 //   nome: "Honda",
 // };
 // console.log(honda instanceof Carro);
+const link = document.getElementById("origamid");
+if (link instanceof HTMLAnchorElement) {
+    link.href = link.href.replace("http", "https");
+}
