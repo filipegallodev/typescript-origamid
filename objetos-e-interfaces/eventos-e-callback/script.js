55c8f365c88766bfa058a808f2df8298e626c06e
Filipe <fgallo_dev@hotmail.com>
2023-01-09 08:38:02 -0300

    Eventos e Callback 2

--- objetos-e-interfaces/eventos-e-callback/script.js
+++ objetos-e-interfaces/eventos-e-callback/script.js
@@ -1,19 +1,28 @@
 "use strict";
+// const button = document.querySelector("button");
+// function handleClick(event: PointerEvent) {
+//   console.log(event);
+// }
+// button?.addEventListener("pointerdown", handleClick);
+// function handleScroll(event: Event) {
+//   console.log(event);
+// }
+// window.addEventListener("scroll", handleScroll);
+// function ativarMenu(event: Event) {
+//   if (event instanceof MouseEvent) console.log(event.pageX);
+//   if (event instanceof TouchEvent) console.log(event.touches[0].pageX);
+// }
+// document.documentElement.addEventListener("mousedown", ativarMenu);
+// document.documentElement.addEventListener("touchstart", ativarMenu);
+// window.addEventListener("keydown", ativarMenu);
+// const button = document.querySelector("button");
+// function handleClick(this: HTMLButtonElement, event: MouseEvent) {
+//   console.log(this);
+// }
+// button?.addEventListener("click", handleClick);
 const button = document.querySelector("button");
 function handleClick(event) {
-    console.log(event);
+    if (event.currentTarget instanceof HTMLElement)
+        console.log(event.currentTarget.innerText);
 }
-button?.addEventListener("pointerdown", handleClick);
-function handleScroll(event) {
-    console.log(event);
-}
-window.addEventListener("scroll", handleScroll);
-function ativarMenu(event) {
-    if (event instanceof MouseEvent)
-        console.log(event.pageX);
-    if (event instanceof TouchEvent)
-        console.log(event.touches[0].pageX);
-}
-document.documentElement.addEventListener("mousedown", ativarMenu);
-document.documentElement.addEventListener("touchstart", ativarMenu);
-window.addEventListener("keydown", ativarMenu);
+button?.addEventListener("click", handleClick);
