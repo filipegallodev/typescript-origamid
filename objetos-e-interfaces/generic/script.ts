f93f8ae776f566bd82ec2a635c22dc3798f04ac0
Filipe <fgallo_dev@hotmail.com>
2023-01-09 10:24:15 -0300

    Generic 1

---
+++ objetos-e-interfaces/generic/script.ts
@@ -0,0 +1,38 @@
+// function retorno<variavel>(a: variavel): variavel {
+//   return a;
+// }
+
+// console.log(retorno<string>("A Game"));
+// console.log(retorno<number>(200));
+// console.log(retorno<boolean>(true));
+
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const frutas = ["Banana", "Pera", "Uva", "Laranja", "Limão", "Maçã"];
+
+// function firstFive<T>(lista: T[]): T[] {
+//   return lista.slice(0, 5);
+// }
+
+// console.log(firstFive(numeros));
+// console.log(firstFive(frutas));
+
+// function notNull<T>(arg: T) {
+//   if (arg !== null) return arg;
+//   return null;
+// }
+
+// console.log(notNull("André"));
+// console.log(notNull(200));
+
+function tipoDado<T>(a: T): { dado: T; tipo: string } {
+  const resultado = {
+    dado: a,
+    tipo: typeof a,
+  };
+  console.log(resultado);
+  return resultado;
+}
+
+tipoDado("Teste");
+tipoDado(true);
+tipoDado(200);
