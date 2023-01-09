95584d030333036a9bd70a87e2dec17d69c3aac4
Filipe <fgallo_dev@hotmail.com>
2023-01-09 13:58:20 -0300

    Functions 2

--- objetos-e-interfaces/functions/script.js
+++ objetos-e-interfaces/functions/script.js
@@ -1,20 +1,17 @@
 "use strict";
-function somar(a, b, c) {
-    return a + b + (c ? c : 0);
+// function somar(a: number, b: number, c?: number): number {
+//   return a + b + (c ? c : 0);
+// }
+function normalizar(valor) {
+    if (typeof valor === "string")
+        return valor.trim().toLowerCase();
+    return valor.map((item) => item.trim().toLowerCase());
 }
-console.log(somar(3, 4, 6));
-const subtrair = (a, b) => a - b;
-console.log(subtrair(5, 2));
-function pintarTela(cor) {
-    document.body.style.background = cor;
+console.log(normalizar(" Produto "));
+console.log(normalizar([" Banana", " UVA   "]));
+function $(seletor) {
+    return document.querySelector(seletor);
 }
-console.log(pintarTela("black"));
-const btn = document.querySelector("button");
-btn?.click();
-function isString(value) {
-    if (typeof value === "string") {
-        return true;
-    }
-}
-console.log(isString("Teste"));
-console.log(isString(200));
+$("a");
+$("video");
+$(".item");
--- objetos-e-interfaces/functions/script.ts
+++ objetos-e-interfaces/functions/script.ts
@@ -1,30 +1,68 @@
-function somar(a: number, b: number, c?: number): number {
-  return a + b + (c ? c : 0);
+// function somar(a: number, b: number, c?: number): number {
+//   return a + b + (c ? c : 0);
+// }
+
+// console.log(somar(3, 4, 6));
+
+// const subtrair = (a: number, b: number) => a - b;
+
+// console.log(subtrair(5, 2));
+
+// type Callback = (event: MouseEvent) => void;
+
+// function pintarTela(cor: string): void {
+//   document.body.style.background = cor;
+// }
+
+// console.log(pintarTela("black"));
+
+// const btn = document.querySelector("button");
+
+// btn?.click();
+
+// function isString(value: any) {
+//   if (typeof value === "string") {
+//     return true;
+//   }
+// }
+
+// console.log(isString("Teste"));
+// console.log(isString(200));
+
+// function abortar(mensagem: string): never {
+//   throw new Error(mensagem);
+// }
+
+// abortar("Um erro ocorreu");
+// console.log("Tente novamente");
+
+// interface Quadrado {
+//   lado: number;
+//   perimetro(lado: number): number;
+// }
+
+// function calcular(forma: Quadrado) {
+//   forma.perimetro(3);
+// }
+
+function normalizar(valor: string): string;
+function normalizar(valor: string[]): string[];
+function normalizar(valor: string | string[]): string | string[] {
+  if (typeof valor === "string") return valor.trim().toLowerCase();
+
+  return valor.map((item) => item.trim().toLowerCase());
 }
 
-console.log(somar(3, 4, 6));
+console.log(normalizar(" Produto "));
+console.log(normalizar([" Banana", " UVA   "]));
 
-const subtrair = (a: number, b: number) => a - b;
-
-console.log(subtrair(5, 2));
-
-type Callback = (event: MouseEvent) => void;
-
-function pintarTela(cor: string): void {
-  document.body.style.background = cor;
+function $(seletor: "a"): HTMLAnchorElement | null;
+function $(seletor: "video"): HTMLVideoElement | null;
+function $(seletor: string): Element | null;
+function $(seletor: string): Element | null {
+  return document.querySelector(seletor);
 }
 
-console.log(pintarTela("black"));
-
-const btn = document.querySelector("button");
-
-btn?.click();
-
-function isString(value: any) {
-  if (typeof value === "string") {
-    return true;
-  }
-}
-
-console.log(isString("Teste"));
-console.log(isString(200));
+$("a");
+$("video");
+$(".item");
