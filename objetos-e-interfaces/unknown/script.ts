573c688cb52dce159d2285a850b8d86e0d69b77c
Filipe <filipegallodev@hotmail.com>
2023-01-10 09:39:18 -0300

    Unknown

---
+++ objetos-e-interfaces/unknown/script.ts
@@ -0,0 +1,11 @@
+function typeGuard(value: unknown) {
+  if (typeof value === "string") return value.toLowerCase();
+
+  if (typeof value === "number") return value.toFixed();
+
+  if (value instanceof HTMLElement) return value.innerText;
+}
+
+console.log(typeGuard(250));
+console.log(typeGuard("Testando Type Guard e Unknown"));
+console.log(typeGuard(document.body));
