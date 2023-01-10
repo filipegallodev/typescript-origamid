808741c1d3d0dca6f5a225dda505f1dafbd7ae81
Filipe <filipegallodev@hotmail.com>
2023-01-10 09:20:26 -0300

    Type Guard e Control Flow

---
+++ objetos-e-interfaces/type-guard-e-control-flow/script.ts
@@ -0,0 +1,41 @@
+// function typeGuard(value: any) {
+//   if (typeof value === "string") return value.toLowerCase();
+
+//   if (typeof value === "number") return value.toFixed();
+
+//   if (value instanceof HTMLElement) return value.innerText;
+// }
+
+// console.log(typeGuard(200));
+// console.log(typeGuard("Texto Qualquer"));
+// console.log(typeGuard(document.body));
+
+const obj = {
+  nome: "Origamid",
+};
+
+if ("nome" in obj) {
+  console.log("Sim");
+}
+
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const json = await response.json();
+  handleProduto(json);
+}
+
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+function handleProduto(data: Produto) {
+  if ("nome" in data && "preco" in data) {
+    document.body.innerHTML += `
+    <p>Nome: ${data.nome}</p>
+    <p>Preço: R$ ${data.preco + 100}</p>
+    `;
+  }
+}
+
+fetchProduto();
