4b2445a8ca11961967564a844f40406ebc50a307
Filipe <fgallo_dev@hotmail.com>
2023-01-11 08:13:08 -0300

    User Type Guard 2

--- objetos-e-interfaces/user-type-guard/script.js
+++ objetos-e-interfaces/user-type-guard/script.js
@@ -1,24 +1,49 @@
 "use strict";
-async function fetchCursos() {
-    const response = await fetch("https://api.origamid.dev/json/cursos.json");
+// async function fetchCursos() {
+//   const response = await fetch("https://api.origamid.dev/json/cursos.json");
+//   const json = await response.json();
+//   handleCursos(json);
+// }
+// fetchCursos();
+// function handleCursos(data: unknown) {
+//   if (data instanceof Array) {
+//     console.log("É uma instância de Array!");
+//   }
+//   if (Array.isArray(data)) {
+//     console.log("É Array!");
+//   }
+// }
+// function isString(value: unknown): value is string {
+//   return typeof value === "string";
+// }
+// function handleData(data: unknown) {
+//   if (isString(data)) {
+//     console.log(data.toLowerCase());
+//   }
+// }
+// handleData("Origamid");
+async function fetchProduto() {
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
     const json = await response.json();
-    handleCursos(json);
+    handleProduto(json);
 }
-fetchCursos();
-function handleCursos(data) {
-    if (data instanceof Array) {
-        console.log("É uma instância de Array!");
+fetchProduto();
+function isProduto(value) {
+    if (value &&
+        typeof value === "object" &&
+        "nome" in value &&
+        "preco" in value) {
+        return true;
     }
-    if (Array.isArray(data)) {
-        console.log("É Array!");
+    return false;
+}
+function handleProduto(data) {
+    if (isProduto(data)) {
+        if (typeof data.nome === "string") {
+            console.log(data.nome);
+        }
+        if (typeof data.preco === "number") {
+            console.log(data.preco);
+        }
     }
 }
-function isString(value) {
-    return typeof value === "string";
-}
-function handleData(data) {
-    if (isString(data)) {
-        console.log(data.toLowerCase());
-    }
-}
-handleData("Origamid");
--- objetos-e-interfaces/user-type-guard/script.ts
+++ objetos-e-interfaces/user-type-guard/script.ts
@@ -1,28 +1,65 @@
-async function fetchCursos() {
-  const response = await fetch("https://api.origamid.dev/json/cursos.json");
+// async function fetchCursos() {
+//   const response = await fetch("https://api.origamid.dev/json/cursos.json");
+//   const json = await response.json();
+//   handleCursos(json);
+// }
+// fetchCursos();
+
+// function handleCursos(data: unknown) {
+//   if (data instanceof Array) {
+//     console.log("É uma instância de Array!");
+//   }
+
+//   if (Array.isArray(data)) {
+//     console.log("É Array!");
+//   }
+// }
+
+// function isString(value: unknown): value is string {
+//   return typeof value === "string";
+// }
+
+// function handleData(data: unknown) {
+//   if (isString(data)) {
+//     console.log(data.toLowerCase());
+//   }
+// }
+
+// handleData("Origamid");
+
+async function fetchProduto() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
   const json = await response.json();
-  handleCursos(json);
+  handleProduto(json);
 }
-fetchCursos();
+fetchProduto();
 
-function handleCursos(data: unknown) {
-  if (data instanceof Array) {
-    console.log("É uma instância de Array!");
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+function isProduto(value: unknown): value is Produto {
+  if (
+    value &&
+    typeof value === "object" &&
+    "nome" in value &&
+    "preco" in value
+  ) {
+    return true;
   }
 
-  if (Array.isArray(data)) {
-    console.log("É Array!");
+  return false;
+}
+
+function handleProduto(data: unknown) {
+  if (isProduto(data)) {
+    if (typeof data.nome === "string") {
+      console.log(data.nome);
+    }
+
+    if (typeof data.preco === "number") {
+      console.log(data.preco);
+    }
   }
 }
-
-function isString(value: unknown): value is string {
-  return typeof value === "string";
-}
-
-function handleData(data: unknown) {
-  if (isString(data)) {
-    console.log(data.toLowerCase());
-  }
-}
-
-handleData("Origamid");
