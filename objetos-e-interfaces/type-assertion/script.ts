e625307727f16c3e78c203cb733bb743bb79d309
Filipe <fgallo_dev@hotmail.com>
2023-01-11 09:13:22 -0300

    Type Assertion

---
+++ objetos-e-interfaces/type-assertion/script.ts
@@ -0,0 +1,30 @@
+// const video = document.querySelector("#video") as HTMLVideoElement;
+
+// console.log(video.volume);
+
+// interface Produto {
+//   nome: string;
+//   preco: number;
+// }
+
+// async function fetchProduto() {
+//   const response = await fetch("https://api.origamid.dev/json/notebook.json");
+//   return response.json() as Promise<Produto>;
+// }
+
+// async function handleProduto() {
+//   const produto = await fetchProduto();
+//   console.log(produto);
+// }
+// handleProduto();
+
+// const video = document.querySelector("video")!;
+
+// console.log(video.volume);
+
+// document.querySelector("a")!.href = "https://www.origamid.com";
+
+const video = document.querySelector(".video");
+const video1 = document.querySelector(".video") as HTMLVideoElement;
+const video2 = <HTMLVideoElement>document.querySelector(".video");
+const video3 = document.querySelector<HTMLVideoElement>(".video");
