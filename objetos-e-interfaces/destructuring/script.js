791b9dabca3ee1eaea4492684b2882807bb6ca3c
Filipe <fgallo_dev@hotmail.com>
2023-01-11 09:32:34 -0300

    Destructuring 1

---
+++ objetos-e-interfaces/destructuring/script.js
@@ -0,0 +1,36 @@
+"use strict";
+// const { body }: { body: HTMLElement } = document;
+// Caso o tipo não venha informado na desestruturação
+// const { body } = document;
+// interface Produto {
+//   nome: string;
+//   preco?: number;
+// }
+// function handleData({ nome, preco }: Produto) {
+//   nome.includes("book");
+//   preco?.toFixed();
+// }
+// handleData({
+//   nome: "Notebook",
+//   preco: 2000,
+// });
+function handleClick1({ currentTarget, pageX, }) {
+    if (currentTarget instanceof HTMLElement) {
+        currentTarget.innerHTML = `<h1>Mouse click em X: ${pageX}</h1>`;
+    }
+    console.log(pageX);
+}
+function handleClick2({ currentTarget, pageX }) {
+    if (currentTarget instanceof HTMLElement) {
+        currentTarget.innerHTML = `<h1>Mouse click em X: ${pageX}</h1>`;
+    }
+    console.log(pageX);
+}
+document.documentElement.addEventListener("click", handleClick2);
+function handleClick({ currentTarget }) {
+    if (currentTarget instanceof HTMLElement) {
+        currentTarget.innerHTML = `<h1>Mouse click em X: 1</h1>`;
+    }
+}
+document.documentElement.addEventListener("click", handleClick);
+document.documentElement.addEventListener("touchstart", handleClick);
