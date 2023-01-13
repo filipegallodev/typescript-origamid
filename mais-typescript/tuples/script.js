d269c6dd0016175a1f94ee6da6d9f0fe8252e656
Filipe <filipegallodev@hotmail.com>
2023-01-13 08:52:18 -0300

    Tuples 2

--- mais-typescript/tuples/script.js
+++ mais-typescript/tuples/script.js
@@ -7,12 +7,30 @@
 // console.log(produto2[0].toLowerCase());
 // console.log(produto2[1].toFixed());
 // const [nome, preco] = produto2;
-function getText(selector) {
-    const el = document.querySelector(selector);
-    if (el) {
-        return [el, el.innerText];
-    }
-    return null;
+// function getText(selector: string) {
+//   const el = document.querySelector<HTMLElement>(selector);
+//   if (el) {
+//     return [el, el.innerText] as const;
+//   }
+//   return null;
+// }
+// const button = getText("button");
+// console.log(button);
+// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
+async function fetchVendas() {
+    const response = await fetch("https://api.origamid.dev/json/vendas.json");
+    const data = await response.json();
+    somarTotal(data);
 }
-const button = getText("button");
-console.log(button);
+fetchVendas();
+function somarTotal(vendas) {
+    const total = vendas.reduce((total, venda) => total + venda[1], 0);
+    mostrarTotal(total);
+}
+function mostrarTotal(total) {
+    document.body.innerHTML += `
+    <p>Total: R$ ${total}</p>
+  `;
+}
--- mais-typescript/tuples/script.ts
+++ mais-typescript/tuples/script.ts
@@ -10,14 +10,43 @@
 
 // const [nome, preco] = produto2;
 
-function getText(selector: string) {
-  const el = document.querySelector<HTMLElement>(selector);
-  if (el) {
-    return [el, el.innerText] as const;
-  }
+// function getText(selector: string) {
+//   const el = document.querySelector<HTMLElement>(selector);
+//   if (el) {
+//     return [el, el.innerText] as const;
+//   }
 
-  return null;
+//   return null;
+// }
+
+// const button = getText("button");
+// console.log(button);
+
+// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
+
+async function fetchVendas() {
+  const response = await fetch("https://api.origamid.dev/json/vendas.json");
+  const data = await response.json();
+  somarTotal(data);
+}
+fetchVendas();
+
+interface ProdutoDetalhes {
+  marca: string;
+  cor: string;
 }
 
-const button = getText("button");
-console.log(button);
+type Venda = [string, number, string, ProdutoDetalhes];
+
+function somarTotal(vendas: Venda[]) {
+  const total = vendas.reduce((total, venda) => total + venda[1], 0);
+  mostrarTotal(total);
+}
+
+function mostrarTotal(total: number) {
+  document.body.innerHTML += `
+    <p>Total: R$ ${total}</p>
+  `;
+}
