bb0fe0b73301f420e86d3625d551bdc1ecae5eea
Filipe <filipegallodev@hotmail.com>
2023-01-17 08:50:54 -0300

    Bibliotecas Externas

---
+++ desenvolvimento/bibliotecas-externas/dist/script.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="jquery" />
+declare const body: JQuery<HTMLElement>;
---
+++ desenvolvimento/bibliotecas-externas/dist/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const body = $("body");
+console.log(body.addClass("ativo"));
+console.log(_.difference([1, 2, 3, 5, 6], [2, 3, 1, 9]));
+console.log(_.intersection([1, 2, 3, 5, 6], [2, 3, 1, 9]));
+//# sourceMappingURL=script.js.map
